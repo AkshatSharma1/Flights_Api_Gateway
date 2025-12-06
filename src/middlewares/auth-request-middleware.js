1f51ab4707a24c905aa508d0fe212d1f5883c8c5
Akshat Ajay <akshat.ajay0931@gmail.com>
2025-12-06 14:12:13 +0530

    Updated

--- src/middlewares/auth-request-middleware.js
+++ src/middlewares/auth-request-middleware.js
@@ -2,35 +2,45 @@ const axios = require('axios');
 
 const checkAuth = async (req, res, next) => {
     try {
-        // 1. Check if token exists
-        const token = req.headers['x-access-token'];
-        
-        if(!token) {
-            return res.status(401).json({
-                message: 'No token provided. Access denied.'
-            });
-        }
+      // 1. Check if token exists
+      const token = req.headers["x-access-token"];
 
-        // 2. Call Auth Service to validate
-        // We assume Auth Service runs on localhost:3001
-        // In production, use process.env.AUTH_SERVICE_URL
-        const response = await axios.get('http://localhost:3001/api/v1/user/isAuthenticated', {
-            headers: {
-                'x-access-token': token
-            }
+      if (!token) {
+        return res.status(401).json({
+          message: "No token provided. Access denied.",
         });
+      }
 
-        // 3. If successful, attach user info to the request header
-        // This passes the User ID to the downstream services (Flight/Booking)
-        if(response.data.success) {
-            req.headers['x-user-id'] = response.data.data; // 'data' is the userId from auth service
-            next();
-        } else {
-            return res.status(401).json({
-                message: 'Invalid token.'
-            });
+      // 2. Call Auth Service to validate
+      // We assume Auth Service runs on localhost:3001
+      // In production, use process.env.AUTH_SERVICE_URL
+      // const response = await axios.get('http://localhost:3001/api/v1/user/isAuthenticated', {
+      //     headers: {
+      //         'x-access-token': token
+      //     }
+      // });
+
+      //For docker prod
+      const AUTH_URL = process.env.AUTH_SERVICE_URL || "http://localhost:3001";
+      const response = await axios.get(
+        `${AUTH_URL}/api/v1/user/isAuthenticated`,
+        {
+          headers: {
+            "x-access-token": token,
+          },
         }
+      );
 
+      // 3. If successful, attach user info to the request header
+      // This passes the User ID to the downstream services (Flight/Booking)
+      if (response.data.success) {
+        req.headers["x-user-id"] = response.data.data; // 'data' is the userId from auth service
+        next();
+      } else {
+        return res.status(401).json({
+          message: "Invalid token.",
+        });
+      }
     } catch (error) {
         return res.status(401).json({
             message: 'Unauthorized. Token validation failed.',
